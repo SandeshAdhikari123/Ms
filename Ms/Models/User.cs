764658f6d63b845965e1cbfe069f5b7217c23858
Sandesh Adhikari <saa4007@my.londonmet.ac.uk>
2025-01-17 01:50:48 +0545

    added login and register

--- Ms/Models/User.cs
+++ Ms/Models/User.cs
@@ -8,11 +8,10 @@ namespace Ms.Models
 {
     public class User
     {
-        public int Id { get; set; } // Unique identifier for the user
-        public string Name { get; set; } // Name of the user
-        public string Email { get; set; } // Email address
-        public string Phone { get; set; } // Contact number
-        public string Address { get; set; } // Address (optional)
+
+        public string Username { get; set; }  // This is required for the registration logic
+        public string Password { get; set; }  // This will store the hashed password
+        public string Email { get; set; }
         public DateTime CreatedOn { get; set; } = DateTime.Now; // Date the user was added
     }
 
